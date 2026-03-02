1cb1687dd3c43bfa325dc906f09233736a168566
Yashgupta0602 <yashg6274@gmail.com>
2026-03-02 18:09:27 +0530

    Implement max subarray sum using sliding window

---
+++ Sliding Window/Max Sum Subarray of size K.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        int windowSum = 0;
+        for(int i=0;i<k;i++){
+            windowSum = windowSum + arr[i];
+        }
+        
+        int maxSum = windowSum;
+        for(int i=k;i<arr.length;i++){
+            windowSum = windowSum + arr[i] - arr[i-k];
+            maxSum = Math.max(windowSum,maxSum);
+            
+        }
+        return maxSum;
+    }
+}
