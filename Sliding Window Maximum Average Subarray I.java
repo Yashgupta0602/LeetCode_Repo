4f2669ccb575b638aaed746f45080e620ebcfbd6
Yashgupta0602 <yashg6274@gmail.com>
2026-03-02 18:26:12 +0530

    Add solution for maximum average subarray using sliding window
    Implement sliding window algorithm to find maximum average of subarray.

---
+++ Sliding Window Maximum Average Subarray I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double windowSum = 0;
+        for(int i = 0;i<k;i++){
+            windowSum = windowSum + nums[i];
+        }
+        double maxSum= windowSum;
+
+        for(int i=k;i<nums.length;i++){
+            windowSum = windowSum + nums[i] -nums[i-k];
+            maxSum = Math.max(windowSum,maxSum);
+        }
+        return (maxSum/k);
+    }
+}
