4b880356f79a9f63b27a6640c5472943e2b8a34c
Yashgupta0602 <yashg6274@gmail.com>
2026-05-26 15:59:53 +0530

    Add solution to count special quadruplets

---
+++ Brute Force/Count Special Quadruplets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countQuadruplets(int[] nums) {
+        int count = 0;
+        for(int i = 0; i< nums.length-3; i++){
+            
+            for(int  j = i +1; j< nums.length-2; j++){
+                
+                for(int k = j+1; k <nums.length-1; k++){
+                    
+                    for(int x = k+1; x<nums.length; x++){
+                    
+                        if(nums[i] + nums[j] + nums[k] == nums[x]){
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
