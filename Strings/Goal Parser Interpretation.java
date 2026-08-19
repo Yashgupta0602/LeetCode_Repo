221fb6e6a40038276c92c54fb08904d8285c16c5
Yashgupta0602 <yashg6274@gmail.com>
2026-08-19 16:22:02 +0530

    Delete Strings/Goal Parser Interpretation.java

--- Strings/Goal Parser Interpretation.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public String interpret(String command) {
-       command = command.replace("()","o");
-        command = command.replace("(al)","al");
-        return command;
-    }
-}
